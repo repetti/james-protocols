aeb7a84be72abb21cbac2ee65d8ea6d074ca1669
Eric Charles <eric@apache.org>
2012-12-09 06:55:20 +0000

    MoveProcessor implements CapabilityImplementingProcessor, patch by Andrzej Rusin (IMAP-370)
    git-svn-id: https://svn.apache.org/repos/asf/james/protocols/trunk@1418826 13f79535-47bb-0310-9956-ffa450edef68

--- imap/src/main/java/org/apache/james/imap/processor/MoveProcessor.java
+++ imap/src/main/java/org/apache/james/imap/processor/MoveProcessor.java
@@ -1,9 +1,13 @@
 package org.apache.james.imap.processor;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.james.imap.api.ImapConstants;
 import org.apache.james.imap.api.message.response.StatusResponseFactory;
 import org.apache.james.imap.api.process.ImapProcessor;
+import org.apache.james.imap.api.process.ImapSession;
 import org.apache.james.imap.api.process.SelectedMailbox;
 import org.apache.james.imap.message.request.MoveRequest;
 import org.apache.james.mailbox.MailboxManager;
@@ -12,7 +16,9 @@ import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.mailbox.model.MessageRange;
 
-public class MoveProcessor extends CopyProcessor {
+public class MoveProcessor extends CopyProcessor implements CapabilityImplementingProcessor {
+
+	private static final List<String> CAPS = Collections.unmodifiableList(Arrays.asList(ImapConstants.MOVE_COMMAND_NAME));
 
 	public MoveProcessor(ImapProcessor next, MailboxManager mailboxManager,
 			StatusResponseFactory factory) {
@@ -26,6 +32,14 @@ public class MoveProcessor extends CopyProcessor {
 			throws MailboxException {
 		List<MessageRange> processedUids = mailboxManager.moveMessages(messageSet, currentMailbox.getPath(), targetMailbox, mailboxSession);
 		return processedUids;
+	}
+
+    /**
+    * @see org.apache.james.imap.processor.CapabilityImplementingProcessor
+    * #getImplementedCapabilities(org.apache.james.imap.api.process.ImapSession)
+    */
+	public List<String> getImplementedCapabilities(ImapSession session) {
+		return CAPS;
 	}
 
 }
